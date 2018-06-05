e7a0803892ff53129baa38cf24d395110d968128
Mark Miyashita <negativetwelve@gmail.com>
2018-06-04 21:55:43 -0700

    Add main script to get latest block data from eth test net

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Libraries
+const Web3 = require('web3');
+const {Web3Wrapper} = require('@0xproject/web3-wrapper');
+
+const INFURA_URL = 'https://ropsten.infura.io';
+
+const run = async () => {
+  const provider = new Web3.providers.HttpProvider(`${INFURA_URL}/${process.env.INFURA_KEY}`);
+  const client = new Web3Wrapper(provider);
+  const blockNumber = await client.getBlockNumberAsync();
+  console.log(`Latest block number: ${blockNumber}`);
+
+  const block = await client.getBlockAsync(blockNumber);
+  console.log(`Latest block: ${JSON.stringify(block)}`);
+};
+
+run();
